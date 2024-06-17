ded74409337c2de56bac7ecf53bb74b8d7936bc3
anhtuankckt <anhtuankckt@gmail.com>
2024-06-17 12:14:53 +0700

    initial project

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+import styles from "../page.module.css";
+
+export default function AboutPage() {
+  return (
+    <main className={styles.main}>
+      <div>About Page</div>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda iusto, harum ratione velit autem magnam vero praesentium nobis blanditiis excepturi quod a officiis iure, tempora mollitia, et corporis in. Cum?</p>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi veniam dolore a inventore et provident? Ipsum delectus consequuntur voluptatibus eligendi officiis quisquam corrupti dicta, nesciunt, possimus illum, perferendis natus quidem!</p>
+    </main>
+  );
+}
